c91f2e4b7177cb81b29000567b6d8267410337b0
PhungNV <phungnv.bkdn@gmail.com>
2019-09-16 07:16:55 +0700

    migrate SWPrecache to Workbox

--- next.config.js
+++ next.config.js
@@ -1,17 +1,28 @@
-const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+const { GenerateSW } = require('workbox-webpack-plugin');
+// const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   webpack: config => {
     config.plugins.push(
-      new SWPrecacheWebpackPlugin({
-        minify: true,
-        staticFileGlobsIgnorePatterns: [/\.next\//],
-        runtimeCaching: [
-          {
-            handler: "networkFirst",
-            urlPattern: /^https?.*/
-          }
-        ]
+      new GenerateSW({
+        // Do not precache images
+        exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+        // minify: true,
+        // globPatternsIgnorePatterns: [/\.next\//],
+        runtimeCaching: [{
+          urlPattern: /^https?.*/,
+          // 'fastest' is now 'StaleWhileRevalidate'
+          handler: 'NetworkFirst',
+          options: {
+            // options.cache.name is now options.cacheName
+            cacheName: 'my-hackernews-api-cache',
+            // options.cache is now options.expiration
+            expiration: {
+              maxEntries: 5,
+              maxAgeSeconds: 60,
+            },
+          },
+        }],
       })
     );
 
